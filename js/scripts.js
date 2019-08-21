c39a7ea4352db35cd173a0f29d54739c75adab06
Cynthia Ponce and Victor Guttmann <cynthia@>
2019-08-21 14:03:40 -0700

    Add updated for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   $("form.form").submit(function(event) {
     var groceryList = ["item1", "item2", "item3", "item4", "item5"];
+    var emptyList= []
+    emptyList.push($("#item1").val());
+    emptyList.push($("#item2").val());
+    emptyList.push($("#item3").val());
+    emptyList.push($("#item4").val());
+    emptyList.push($("#item5").val());
 
-    for (var index = 0; index < groceryList.length; index += 1) {
+    for (var index = 0; index < emptyList.length; index += 1) {
+      $("ul#list").append("<li>" + emptyList[index] + "</li>");
 
-
-      alert("Here is your list:" + groceryList[index]);
     }
 
-
-  //  for (var userInput = 0; userInput < "input#" + )
 
 
 
@@ -19,16 +22,17 @@ $(document).ready(function() {
 
   event.preventDefault();
 
-/* This displays refactoring with Loops
+/* This displays refactoring with forEach
 
-$(document).ready(function() {
-  $("form.form").submit(function(event) {
-    var groceryList = ["item1", "item2", "item3", "item4", "item5"];
+            $(document).ready(function() {
+              $("form.form").submit(function(event) {
+                var groceryList = ["item1", "item2", "item3", "item4", "item5"];
 
-    groceryList.forEach(function(inputVal) {
-    var userInput = $("input#" + inputVal).val();
-    $("#" + inputVal + "Answer").text(userInput);
-    });
+                groceryList.forEach(function(inputVal) {
+                var userInput = $("input#" + inputVal).val();
+                $("#" + inputVal + "Answer").text(userInput);
+                });
+
 */
 
 
@@ -36,20 +40,20 @@ $(document).ready(function() {
 
     /*  This is the original method of collecting the user input and later displaying it (not DRY code)
 
-    $(document).ready(function() {
-      $("form.form").submit(function(event) {
-      var item1Response = $("input#item1").val();
-      var item2Response = $("input#item2").val();
-      var item3Response = $("input#item3").val();
-      var item4Response = $("input#item4").val();
-      var item5Response = $("input#item5").val();
+                  $(document).ready(function() {
+                    $("form.form").submit(function(event) {
+                    var item1Response = $("input#item1").val();
+                    var item2Response = $("input#item2").val();
+                    var item3Response = $("input#item3").val();
+                    var item4Response = $("input#item4").val();
+                    var item5Response = $("input#item5").val();
 
-      $("#summary").show();
-      $("#input1Answer").text(item1Response);
-      $("#input2Answer").text(item2Response);
-      $("#input3Answer").text(item3Response);
-      $("#input4Answer").text(item4Response);
-      $("#input5Answer").text(item5Response);
+                    $("#summary").show();
+                    $("#input1Answer").text(item1Response);
+                    $("#input2Answer").text(item2Response);
+                    $("#input3Answer").text(item3Response);
+                    $("#input4Answer").text(item4Response);
+                    $("#input5Answer").text(item5Response);
 
     */
 
